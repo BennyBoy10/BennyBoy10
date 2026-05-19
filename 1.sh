a6aa90afd274c0bae4445737cfa4b6dc6860ca69
BennyBoy10 <160542234+BennyBoy10@users.noreply.github.com>
2026-05-19 01:30:43 +0100

    Updated Files

---
+++ 1.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
+
+echo ""
+echo -e "\e[32mStarting BennyBoy10... \e[0m"
+echo ""
+
+git add .
+git commit -m "Updated BennyBoy10"
+git push origin main
+
+echo ""
+echo -e "\e[32mDone!! \e[0m"
+echo ""
+
